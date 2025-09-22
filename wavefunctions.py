67a547a276b91b2de6a4132c871cfb8cadae01fd
pavanravindra <pravin4094@gmail.com>
2025-09-22 13:54:55 -0400

    initial implementation of message passing jastrow

--- wavefunctions.py
+++ wavefunctions.py
@@ -847,3 +847,140 @@ class LogThreePavanJastrowRHF(Wavefunction):
         MBJastrow = jnp.sum(selfTerm) + U3
         
         return slaterUp + slaterDown + CYJastrow + MBJastrow
+
+class LogMessagePassingJastrow(Wavefunction):
+    """
+    Slater-Jastrow wavefunction with following specs:
+    - Slater: RHF ground state
+    - Jastrow: Coulomb-Yukawa + neural message passing Jastrow
+
+    TODO:
+    - add in decay on messages if things aren't variational anymore...
+    - richer v_ij (e.g., with sines and magnitude)
+      - but make sure cusp conditions are maintained!
+    """
+    spins : (int,int)
+    L : float
+    T : int
+    hiddenFeatures : int
+    d1 : int
+    d2 : int
+    dv : int
+
+    def setup(self):
+        
+        self.slaterUp = LogSimpleSlater(self.spins[0], self.L)
+        self.slaterDown = LogSimpleSlater(self.spins[1], self.L)
+        self.CYJastrow = LogCYJastrow(self.spins, self.L)
+        
+        self.decay = DecayFunction(self.L)
+
+        self.hi0 = self.param(
+            "hi0",
+            lambda rng : jnp.zeros(self.d1)
+        )
+        self.hij0 = self.param(
+            "hij0",
+            lambda rng : jnp.zeros(self.d2)
+        )
+
+        self.Wqt = [
+            nn.Dense(self.d2 + self.dv, use_bias=False) for _ in range(self.T)
+        ]
+        self.Wkt = [
+            nn.Dense(self.d2 + self.dv, use_bias=False) for _ in range(self.T)
+        ]
+
+        self.Fmt = [
+            (
+                nn.Dense(self.hiddenFeatures),
+                nn.Dense(self.d2 + self.dv)
+            ) for _ in range(self.T)
+        ]
+
+        self.Alineart = [
+            nn.Dense(self.d2 + self.dv) for _ in range(self.T)
+        ]
+
+        self.F1t = [
+            (
+                nn.Dense(self.hiddenFeatures),
+                nn.Dense(self.d1)
+            ) for _ in range(self.T)
+        ]
+        self.F2t = [
+            (
+                nn.Dense(self.hiddenFeatures),
+                nn.Dense(self.d2)
+            ) for _ in range(self.T)
+        ]
+        
+        self.FJt = (
+            nn.Dense(self.hiddenFeatures),
+            nn.Dense(1)
+        )
+
+    def __call__(self, rs):
+        
+        slaterUp = self.slaterUp(rs[:self.spins[0],:])
+        slaterDown = self.slaterDown(rs[self.spins[0]:,:])
+        CYJastrow = self.CYJastrow(rs)
+        
+        disps = rs[:,None,:] - rs[None,:,:]  # (N, N, 3)
+        disps = (disps + self.L/2) % self.L - self.L/2
+        mask = ~jnp.eye(disps.shape[0], dtype=bool)[:,:,None]
+        disps = jnp.where(mask, disps, 0.0)
+        r_ij = jnp.linalg.norm(disps, axis=-1)
+        decays = self.decay(r_ij)[:,:,None]
+        decays = jnp.where(mask, decays, 0.0)
+        
+        cusplessDisps = jnp.cos(jnp.pi * disps / self.L)
+
+        N = self.spins[0] + self.spins[1]
+        electronIdxs = jnp.arange(N)
+        electronSpins = jnp.where(electronIdxs < self.spins[0], 1, -1)
+        matchMatrix = jnp.outer(electronSpins, electronSpins)[:,:,None]
+
+        vij = jnp.concatenate([cusplessDisps, matchMatrix], axis=-1)
+
+        hit = jnp.broadcast_to(self.hi0, (N,self.d1))
+        hijt = jnp.broadcast_to(self.hij0, (N,N,self.d2))
+
+        for t in range(self.T):
+
+            git = hit
+            gijt = jnp.concatenate([hijt,vij], axis=-1)
+
+            qijt = self.Wqt[t](gijt)
+            kijt = self.Wkt[t](gijt)
+            
+            """
+            # Numpy reference implementation
+            np_Aijt = np.zeros((N,N,self.d2+self.dv))
+            for i in range(N):
+                for j in range(N):
+                    for l in range(N):
+                        np_Aijt[i,j,:] += qijt[i,l,:] * kijt[l,j,:] / jnp.sqrt(N)
+            """
+
+            Aijt = self.Alineart[t](
+                nn.swish(
+                    jnp.einsum("ild,ljd->ijd", qijt, kijt) / jnp.sqrt(N)
+                )
+            )
+
+            mijt = Aijt * self.Fmt[t][1](nn.swish(self.Fmt[t][0](gijt)))
+            # TODO: this is where you could just multiply by decays if you want to try decaying the messages...
+            masked_mijt = jnp.where(mask, mijt, 0.0)
+            acc_mijt = jnp.sum(masked_mijt, axis=1)
+            
+            hit += self.F1t[t][1](nn.swish(self.F1t[t][0](
+                jnp.concatenate([acc_mijt,git], axis=-1)
+            )))
+            hijt += self.F2t[t][1](nn.swish(self.F2t[t][0](
+                jnp.concatenate([mijt, gijt], axis=-1)
+            )))
+
+        MPJastrow = jnp.sum(self.FJt[1](nn.swish(self.FJt[0](hit))))
+        
+        return slaterUp + slaterDown + CYJastrow + MPJastrow
