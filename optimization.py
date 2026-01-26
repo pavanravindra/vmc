8b2fcd3e9c744fdf9717077c2a9ce6406a8187ad
pavanravindra <pravin4094@gmail.com>
2026-01-26 16:14:46 -0500

    adding test implementation of SMW SR solver

--- optimization.py
+++ optimization.py
@@ -134,6 +134,7 @@ class StochasticReconfiguration:
         s_jk = cov - jnp.outer(exp_O,exp_O)
         diagonalMatrix = diagonalShift * jnp.eye(s_jk.shape[0])
         parameterStep = jnp.linalg.solve(s_jk + diagonalMatrix, f_k)
+        
         fisherNorm = jnp.dot(f_k, parameterStep)
         diagonalCorrection = diagonalShift * jnp.linalg.norm(parameterStep)**2
         fisherNorm = jnp.sqrt(
@@ -150,14 +151,38 @@ class StochasticReconfiguration:
         localParameterGrads = self.parameterGrad(parameters, rs)
         return jax.flatten_util.ravel_pytree(localParameterGrads)[0]
 
-class DiagonalStochasticReconfiguration:
+def smw_solve(P, f, lam):
     """
-    Updates parameters using stochastic reconfiguration with a diagonal Fisher
-    information matrix.
+    Given a system of equations:
 
-    NOTE: With this implementation, the `learningRate` parameter passed to
-    `__call__` can either be a float that is applied evenly to every parameter,
-    or a pytree that specifies parameter-specific learning rates.
+        S theta = f     where
+        S = lambda I + P.T @ P
+
+    We are interested in computing theta by inverting S.
+
+    If the shape of P is (W,P) with W << P, the inverse can be computed more
+    efficiently using the Sherman-Morrison-Woodbury formula. It works by
+    keeping things in the W-dimensional space, rather than inverting in the
+    larger P-dimensional space.
+
+    This is particularly useful in SR for NQS, where the number of parameters
+    is almost always larger than the number of walkers.
+
+    Formula:
+    
+        theta = lambda^{-1} f - lambda^{-1} P.T (lambda I + P P.T)^{-1} P f
+    """
+    term1 = f / lam
+    term2 = P.T / lam @ jnp.linalg.solve(
+        lam * jnp.eye(P.shape[0]) + P @ P.T, P @ f
+    )
+    return term1 - term2
+
+class StochasticReconfigurationSMW:
+    """
+    Updates parameters using stochastic reconfiguration using the
+    Sherman-Morrison-Woodbury formula (useful when the number of parameters is
+    greater than the number of walkers).
     """
 
     def __init__(self, logWavefunction, localEnergy):
@@ -167,29 +192,33 @@ class DiagonalStochasticReconfiguration:
             self.getParameterGradient, in_axes=(None,0)
         )
 
-    def __call__(self, parameters, walkerRs, learningRate, diagonalShift=0.):
+    def __call__(self, parameters, walkerRs, learningRate, diagonalShift):
         
-        if isinstance(learningRate, (float, int)):
-            learningRateTree = castFloatAsPytree(learningRate, parameters)
-        else:
-            learningRateTree = learningRate
-
         localEnergies = self.localEnergy.batch(parameters, walkerRs)
         parameterGrads = self.parameterGradBatchFlat(parameters, walkerRs)
         (flatParameters,unravel) = jax.flatten_util.ravel_pytree(parameters)
-        (learningRateFlat,_) = jax.flatten_util.ravel_pytree(learningRateTree)
-        
+        numWalkers = parameterGrads.shape[0]
+
         exp_H = jnp.average(localEnergies)
         exp_O = jnp.average(parameterGrads, axis=0)
         exp_OH = jnp.average(localEnergies[:,None] * parameterGrads, axis=0)
 
         f_k = 2 * (exp_O * exp_H - exp_OH)
-
-        s_jk = jnp.average(parameterGrads ** 2, axis=0) - (exp_O ** 2)
-        parameterStep = learningRateFlat * f_k / (s_jk + diagonalShift)
-        updatedParameters = unravel(flatParameters + parameterStep)
+        U = parameterGrads - jnp.average(parameterGrads, axis=0, keepdims=True)
+        P = U / jnp.sqrt(numWalkers)
+        parameterStep = smw_solve(P, f_k, diagonalShift)
         
-        return updatedParameters
+        fisherNorm = jnp.dot(f_k, parameterStep)
+        diagonalCorrection = diagonalShift * jnp.linalg.norm(parameterStep)**2
+        fisherNorm = jnp.sqrt(
+            jnp.maximum(fisherNorm - diagonalCorrection, 1e-8)
+        )
+        scale = jnp.minimum(1.0, 1.0 / fisherNorm)
+        scale = jnp.minimum(scale, 1.0 / jnp.linalg.norm(parameterStep))
+        parameterStep = scale * learningRate * parameterStep
+        updatedParameters = unravel(flatParameters + parameterStep)
+
+        return ( scale < 1.0 , localEnergies , updatedParameters )
 
     def getParameterGradient(self, parameters, rs):
         localParameterGrads = self.parameterGrad(parameters, rs)
