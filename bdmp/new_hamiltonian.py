b946c69645b80bd5609a4caa7ea2ee9f92df474a
Pavan Ravindra <phr2114@columbia.edu>
2026-02-25 18:47:50 -0500

    got improved two-body SJB results with node-level aggregation and attention gating (refer to )

---
+++ bdmp/new_hamiltonian.py
@@ -0,0 +1,193 @@
+import jax
+import jax.numpy as jnp
+
+import itertools
+
+class LocalEnergy:
+    
+    def batch(self, parameters, walkerRs):
+        batchFunction = jax.vmap(self.configuration, in_axes=(None,0))
+        return batchFunction(parameters, walkerRs)
+
+class LocalKineticEnergy(LocalEnergy):
+    """
+    Computes the local kinetic energy of an arbitrary log wavefunction.
+
+    TODO: Add formula for convenience...
+    """
+
+    def __init__(self, logWavefunction):
+        self.logWavefunction = logWavefunction.apply
+
+    def configuration(self, parameters, rs):
+
+        def f_rs(x):
+            return self.logWavefunction(parameters, x)
+
+        gradFunction = jax.grad(f_rs)
+        primals, hvp = jax.linearize(gradFunction, rs)
+
+        gradf2 = jnp.sum(primals ** 2)
+
+        D = rs.size
+
+        def for_body(i, acc):
+            e_i = jax.nn.one_hot(i, D, dtype=rs.dtype)
+            He = hvp(e_i.reshape(rs.shape)).reshape(-1)
+            return acc + He[i]
+
+        def map_body(i):
+            e_i = jax.nn.one_hot(i, D, dtype=rs.dtype)
+            He = hvp(e_i.reshape(rs.shape)).reshape(-1)
+            return He[i]
+
+        lap = jax.lax.fori_loop(0, D, for_body, jnp.array(0.0, dtype=rs.dtype))
+        #lap = jnp.sum(jax.vmap(map_body)(jnp.arange(D)))
+
+        kineticEnergy = -0.5 * (gradf2 + lap)
+        
+        return kineticEnergy
+
+class EwaldPotential(LocalEnergy):
+    """
+    This is heavily inspired by code from `make_ewald_potential` in FermiNet!
+    
+    Evaluates the infinite Coulomb sum for a bunch of electrons on a periodic lattice.
+    """
+    
+    def __init__(self, lattice, truncationLimit=2):
+
+        self.lattice = lattice
+        self.truncationLimit = truncationLimit
+        
+        self.rec = 2 * jnp.pi * jnp.linalg.inv(self.lattice)
+        self.volume = jnp.abs(jnp.linalg.det(lattice))
+        self.gamma = (2.8 / self.volume**(1/3)) ** 2
+        
+        ordinals = sorted(range(-truncationLimit, truncationLimit + 1), key=abs)
+        ordinals = jnp.array(list(itertools.product(ordinals, repeat=3)))
+        
+        self.lat_vectors = jnp.einsum('kj,ij->ik', self.lattice, ordinals)
+        lat_vec_norm = jnp.linalg.norm(self.lat_vectors[1:], axis=-1)
+        
+        self.rec_vectors = jnp.einsum('jk,ij->ik', self.rec, ordinals[1:])
+        self.rec_vec_square = jnp.einsum('ij,ij->i', self.rec_vectors, self.rec_vectors)
+                
+        self.madelung_const = (
+            jnp.sum(jax.scipy.special.erfc(jnp.sqrt(self.gamma) * lat_vec_norm) / lat_vec_norm)
+            - 2 * jnp.sqrt(self.gamma) / jnp.sqrt(jnp.pi)
+            + (4 * jnp.pi / self.volume) * jnp.sum(jnp.exp(-self.rec_vec_square / (4 * self.gamma)) / self.rec_vec_square)
+            - jnp.pi / (self.volume * self.gamma)
+        )
+    
+        self.batch_ewald_sum = jax.vmap(self.ewald_sum, in_axes=0)
+    
+    def real_space_ewald(self, separation: jnp.ndarray):
+        disp = jnp.linalg.norm(separation - self.lat_vectors, axis=1)
+        return jnp.sum(jax.scipy.special.erfc(jnp.sqrt(self.gamma) * disp) / disp)
+
+    def recp_space_ewald(self, separation: jnp.ndarray):
+        phase = jnp.dot(self.rec_vectors, separation)
+        factor = jnp.exp(-self.rec_vec_square / (4 * self.gamma)) / self.rec_vec_square
+        return (4 * jnp.pi / self.volume) * jnp.sum(jnp.cos(phase) * factor)
+
+    def ewald_sum(self, separation: jnp.ndarray):
+        return  self.real_space_ewald(separation) +\
+                self.recp_space_ewald(separation) -\
+                jnp.pi / (self.volume * self.gamma)
+
+    def configuration(self, parameters, rs):
+        N = rs.shape[0]
+        ee = rs[:, None, :] - rs[None, :, :]
+        phase_ee = jnp.einsum('il,jkl->jki', self.rec / (2 * jnp.pi), ee)
+        phase_prim_ee = phase_ee % 1
+        prim_ee = jnp.einsum('il,jkl->jki', self.lattice, phase_prim_ee)
+        prim_ee = prim_ee.reshape(-1, 3)
+        vals = self.batch_ewald_sum(prim_ee).reshape(N,N)
+        vals = vals.at[jnp.diag_indices(N)].set(0.0)
+        return 0.5 * jnp.sum(vals) + 0.5 * N * self.madelung_const
+
+class EwaldPotential2D(LocalEnergy):
+    """
+    This is heavily inspired by code from `make_ewald_potential` in FermiNet!
+    
+    Evaluates the infinite Coulomb sum for a bunch of electrons on a periodic lattice
+    in 2 Dimensions.
+    """
+    
+    def __init__(self, lattice, truncationLimit=2):
+
+        self.lattice = lattice
+        self.truncationLimit = truncationLimit
+        
+        self.rec = 2 * jnp.pi * jnp.linalg.inv(self.lattice)
+        self.area = jnp.abs(jnp.linalg.det(lattice))
+        self.gamma = (2.8 / self.area**(1/2)) ** 2
+        
+        ordinals = sorted(range(-truncationLimit, truncationLimit + 1), key=abs)
+        ordinals = jnp.array(list(itertools.product(ordinals, repeat=2)))
+        
+        self.lat_vectors = jnp.einsum('kj,ij->ik', self.lattice, ordinals)
+        lat_vec_norm = jnp.linalg.norm(self.lat_vectors[1:], axis=-1)
+        
+        self.rec_vectors = jnp.einsum('jk,ij->ik', self.rec, ordinals[1:])
+        self.rec_vec_square = jnp.einsum('ij,ij->i', self.rec_vectors, self.rec_vectors)
+        self.rec_vec_norm = jnp.sqrt(self.rec_vec_square)
+                
+        self.madelung_const = (
+            jnp.sum(jax.scipy.special.erfc(jnp.sqrt(self.gamma) * lat_vec_norm) / lat_vec_norm)
+            - 2 * jnp.sqrt(self.gamma) / jnp.sqrt(jnp.pi)
+            + (2 * jnp.pi / self.area) * jnp.sum(jax.scipy.special.erfc(self.rec_vec_norm / (2 * jnp.sqrt(self.gamma))) / self.rec_vec_norm)
+            - 2 * jnp.sqrt(jnp.pi) / (self.area * jnp.sqrt(self.gamma))
+        )
+    
+        self.batch_ewald_sum = jax.vmap(self.ewald_sum, in_axes=0)
+    
+    def real_space_ewald(self, separation: jnp.ndarray):
+        disp = jnp.linalg.norm(separation - self.lat_vectors, axis=1)
+        return jnp.sum(jax.scipy.special.erfc(jnp.sqrt(self.gamma) * disp) / disp)
+
+    def recp_space_ewald(self, separation: jnp.ndarray):
+        phase = jnp.dot(self.rec_vectors, separation)
+        k_norm = jnp.sqrt(jnp.sum(self.rec_vectors**2, axis=1))
+        factor = jax.scipy.special.erfc(k_norm / (2 * jnp.sqrt(self.gamma))) / k_norm
+        return (2 * jnp.pi / self.area) * jnp.sum(jnp.cos(phase) * factor)
+
+    def ewald_sum(self, separation: jnp.ndarray):
+        return  self.real_space_ewald(separation) +\
+                self.recp_space_ewald(separation) -\
+                2 * jnp.sqrt(jnp.pi) / (self.area * jnp.sqrt(self.gamma))
+
+    def configuration(self, parameters, rs):
+        N = rs.shape[0]
+        ee = rs[:, None, :] - rs[None, :, :]
+        phase_ee = jnp.einsum('il,jkl->jki', self.rec / (2 * jnp.pi), ee)
+        phase_prim_ee = phase_ee % 1
+        prim_ee = jnp.einsum('il,jkl->jki', self.lattice, phase_prim_ee)
+        prim_ee = prim_ee.reshape(-1, 2)
+        vals = self.batch_ewald_sum(prim_ee).reshape(N,N)
+        vals = vals.at[jnp.diag_indices(N)].set(0.0)
+        return 0.5 * jnp.sum(vals) + 0.5 * N * self.madelung_const
+
+class LocalEnergyUEG(LocalEnergy):
+
+    def __init__(self, logWavefunction, lattice, truncationLimit=2):
+        self.kineticEnergy = LocalKineticEnergy(logWavefunction)
+        self.potentialEnergy = EwaldPotential(lattice, truncationLimit)
+
+    def configuration(self, parameters, rs):
+        kineticEnergy = self.kineticEnergy.configuration(parameters, rs)
+        potentialEnergy = self.potentialEnergy.configuration(parameters, rs)
+        return kineticEnergy + potentialEnergy
+
+class LocalEnergyUEG2D(LocalEnergy):
+
+    def __init__(self, logWavefunction, lattice, truncationLimit=2):
+        self.kineticEnergy = LocalKineticEnergy(logWavefunction)
+        self.potentialEnergy = EwaldPotential2D(lattice, truncationLimit)
+
+    def configuration(self, parameters, rs):
+        kineticEnergy = self.kineticEnergy.configuration(parameters, rs)
+        potentialEnergy = self.potentialEnergy.configuration(parameters, rs)
+        return kineticEnergy + potentialEnergy
+        
